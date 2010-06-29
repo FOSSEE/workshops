70258fdab869212582e46e39b7794f604a554779
Christopher Burns <chris.d.burns@gmail.com>
2010-06-28 23:17:31 -0500

    REF: Minor formatting tweaks in exercise solutions.
    --HG--
    branch : scipy2010

--- day1/exercise/pytriads.py
+++ day1/exercise/pytriads.py
@@ -8,10 +8,11 @@ def gcd(a, b):
     if a % b == 0:
         return b
     else:
-        return gcd(b, a%b)
+        return gcd(b, a % b)
 
 for a in range(3, 101):
-    for b in range( a+1, 101, 2):
-        if gcd( a, b ) == 1:
+    for b in range(a + 1, 101, 2):
+        if gcd(a, b) == 1:
             is_ps, c = is_perfect_square((a * a) + (b * b))
-            if is_ps: print a, b, c
+            if is_ps:
+                print a, b, c
